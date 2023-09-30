6ef41beb7331aa7b0eb76acb307f7ef20d58f9da
Mohammed Alom <mohammed.1.alom@kcl.ac.uk>
2023-09-30 11:46:28 +0100

    solution

--- primes.py
+++ primes.py
@@ -2,5 +2,21 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+
+    if number_of_primes < 1:
+        raise ValueError
+
+
     list = []
+    number = 2
+    list.append(number)
+    if number_of_primes == 1:
+        return list
+    
+    while len(list) < number_of_primes:
+        if all(number % i != 0 for i in list):
+            list.append(number)
+        number += 1
+        
     return list
+
